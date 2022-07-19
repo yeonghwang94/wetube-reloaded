48ec1d67f177b07e7d132b2c4b40c7c76abfc9cf
Yeongcheol Hwang <60525646+yeonghwang94@users.noreply.github.com>
2022-07-18 20:37:26 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/:id -> See User
+/users/logout -> Log Out
+/users/edit -> Edit My Profile
+/users/delete -> Delete My Profile
+
+/videos/:id -> See Video
+/videos/:id/edit -> Edit a Video
+/videos/upload -> Upload Video
+/videos/delete -> Delete a Video
+
+/videos/comments -> Comment on a Video
+/videos/comments/delete -> Delete a Comment on a Video
---
+++ src/controllers/videoController.js
@@ -0,0 +1,65 @@
+import Video from "../models/Video";
+/*
+Using Callback function:
+Video.find({}, (error, videos) => {
+    return res.render("home", { pageTitled: "Home", videos });
+});
+*/
+
+export const home = async (req, res) => {
+    const videos = await Video.find({});
+    console.log(videos);
+    return res.render("home", { pageTitle: "Home", videos });
+};
+
+export const watch = async (req, res) => {
+    const { id } = req.params;
+    const video = await Video.findById(id);
+    if(!video) {
+        return res.render("404", { pageTitle: "Video not Found" });
+    }
+    return res.render("watch", { pageTitle: video.title, video });
+};
+
+export const getEdit = async (req, res) => {
+    const { id } = req.params;
+    const video = await Video.findById(id);
+    if(!video) {
+        return res.render("404", { pageTitle: "Video not Found" });
+    }
+    return res.render("edit", { pageTitle: `Edit: ${video.title}` , video });
+};
+
+export const postEdit = async (req, res) => {
+    const { id } = req.params;
+    console.log(req.body);
+    const { title, description, hashtags } = req.body;
+    const video = await Video.exists({ _id:id });
+    if(!video) {
+        return res.render("404", { pageTitle: "Video Not Found" });
+    }
+    await Video.findByIdAndUpdate(id, {
+        title,
+        description,
+        hashtags: Video.formatHashtags(hashtags),
+    });
+    return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+    return res.render("upload", {pageTitle: "Upload Video"});
+};
+
+export const postUpload = async (req, res) => {
+    const { title, description, hashtags } = req.body;
+    try {
+        await Video.create({
+            title,
+            description,
+            hashtags: Video.formatHashtags(hashtags),
+        });
+        return res.redirect("/");
+    } catch(error) {
+        return res.render("upload", { pageTitle: "Upload Video", errorMessage: error._message });
+    }
+};
